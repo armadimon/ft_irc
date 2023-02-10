21c26bf71fd04b1c787ee169d42b66f748b3cc95
objectio <objection@kakao.com>
2023-02-10 17:16:34 +0900

    [CHORE] Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,11 +7,11 @@ int main(int argc, char** argv) {
         std::cout << "Usage : ./ircserv <port> <password> " << std::endl;
         exit(1);
     }
-    
     Server server;
     server.setPort(argv[1]);
     server.setPass(argv[2]);
     try {
+        server.start();
     }
     catch (std::exception &e) {
         e.what();
