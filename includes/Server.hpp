37fe9408545ae136487b69fdf006ebd9e3b8beb9
objectio <objection@kakao.com>
2023-02-10 13:58:13 +0900

    [FEAT] Define Server class

---
+++ includes/Server.hpp
@@ -0,0 +1,35 @@
+#ifndef SERVER_HPP
+#define SERVER_HPP
+
+#include <iostream>
+#include <map>
+#include <string>
+#include <netdb.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include "Client.hpp"
+
+#define MAX_FD	1024
+
+class Server
+{
+  public:
+	Server();
+	~Server();
+	Client getClient();
+	void setClient(std::map<int, Client> c);
+	void setPass(char *pw);
+	void setPort(char *port);
+	void createSocket(int port);
+	void acceptClient();
+
+  private:
+	int			fd;
+	int			port;
+	std::map<int, Client> clients;
+	std::string password; // 서버 패스워드
+};
+
+#endif
