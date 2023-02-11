6329688b9d822352197073b23ca2c813fab499ad
Gori <wnsrjf0922@gmail.com>
2023-02-11 16:56:31 +0900

    [FEAT] Add getFD and getBuf

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -5,23 +5,21 @@ Client::Client(int fd)
 {
 }
 
-void Client::clientRead()
-{
-	  int	r;
-
-  	r = recv(fd, bufRead, 1024, 0);
-	  std::cout << bufRead << std::endl;
-  	if (r <= 0)
-    {
-      close(fd);
-      printf("client #%d gone away\n", fd);
-    }
-}
-
 Client::Client()
 { }
 
 Client::~Client()
 {
 
+}
+
+
+char	*Client::getBuf()
+{
+	return (this->bufRead);
+}
+
+int		Client::getFD()
+{
+	return (this->fd);			
 }
