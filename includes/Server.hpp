ce21f0c0427da736bf04b60248c7f89f1812c2fb
Gori <wnsrjf0922@gmail.com>
2023-02-11 15:06:44 +0900

    [FEAT] fix map value (Client -> Client *) & Add var (fd_max, cpy_read_fds, cpy_wirte_fds)

--- includes/Server.hpp
+++ includes/Server.hpp
@@ -31,12 +31,15 @@ class Server
 
   private:
 	int			fd;
+	int			fd_max;
 	int			fd_num;
 	int			port;
 	int			is_set;
+	fd_set		cpy_read_fds;
+	fd_set		cpy_write_fds;
 	fd_set		read_fds;
 	fd_set		write_fds;
-	std::map<int, Client> clients;
+	std::map<int, Client *> clients;
 	std::string password; // 서버 패스워드
 };
 
