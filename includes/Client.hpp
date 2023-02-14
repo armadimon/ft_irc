9e65272d39a607f907fec4c950cbaafe167b0e0f
objectio <objection@kakao.com>
2023-02-14 18:12:07 +0900

    [CHORE] Edit convenvtion

--- includes/Client.hpp
+++ includes/Client.hpp
@@ -26,17 +26,17 @@ class Client
 		Client();
 		Client(int fd);
 		~Client();
-		char	*getBuf();
-		int		getFD();
-		int		parseMSG(std::string tempStr);
-		void	registerClient();
-		void	excute(Server *server);
-		void	setRealName(std::string str);
-		void	setNickName(std::string str);
-		void	setUserName(std::string str);
-		void	setHostName(std::string str);
-		void	setUserState();
-		std::string getUserName();
+		char		*getBuf();
+		int			getFD();
+		int			parseMSG(std::string tempStr);
+		void		registerClient();
+		void		excute(Server *server);
+		void		setRealName(std::string str);
+		void		setNickName(std::string str);
+		void		setUserName(std::string str);
+		void		setHostName(std::string str);
+		void		setUserState();
+		std::string	getUserName();
 		std::string getNickName();
 		std::string getHostName();
 		std::string getRealName();
@@ -45,20 +45,19 @@ class Client
 		// void	setmyChannelList();
 		    // getter, setter
 		// void	partMyChannel(); //vector.erase 클라이언트가 채널을 나갈 때 
-		    //TODO : part명령어가 여러개의 채널을 동시에 떠날 수 있는지 체크하기 
+		    //TODO : part명령어가 여러개의 채널을 동시에 떠날 수 있는지 체크하기
+ 
 	private :
-		// bool	_isOperator; //방장이면 true
-		int		fd;
-		int		passwd;
-		State	userState;
-		std::vector<std::string> msg;
-		std::string hostName;
-		std::string nickName;
-		std::string userName;
-		std::string realName;
-		
-		// std::string
-		char	bufRead[4096];
+		bool						isOperator; //방장이면 true
+		int							fd;
+		int							passwd;
+		State						userState;
+		std::vector<std::string>	msg;
+		std::string					hostName;
+		std::string					nickName;
+		std::string					userName;
+		std::string					realName;
+		char						bufRead[4096];
     // ...
 		//TODO : 사용되는 이름 조사하기
 		// std::vector<Channel> _myChannelList;
