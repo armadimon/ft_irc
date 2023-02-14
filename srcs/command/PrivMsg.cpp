32b04a6fdaf908a24d49f9eb74bbb922b7504618
Gori <wnsrjf0922@gmail.com>
2023-02-14 17:03:21 +0900

    [FEAT] implement PRIVMSG command

---
+++ srcs/command/PrivMsg.cpp
@@ -0,0 +1,28 @@
+#include "../../includes/Server.hpp"
+
+void	cmdPrivMsg(Server *s, int fd, std::vector<std::string> str)
+{
+	std::vector<std::string>::iterator it = str.begin();
+	Client *c = s->getClient(fd);
+
+	std::string temp = ":";
+
+	if (c->getUserState() == REGISTER)
+	{
+		temp += c->getNickName();
+		temp += "!";
+		temp += c->getUserName();
+		temp += "@";
+		temp += c->getHostName();
+		temp += " ";
+		for (; it < str.end(); it++)
+		{
+			temp += *it;
+			// if (cnt > 1)
+			// {
+			// 	if ()
+			// }
+		}
+		send(c->getFD(), temp.c_str(), temp.size(), 0);
+	}
+}
