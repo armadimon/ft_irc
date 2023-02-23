df11a013462e330b12830558387539621b1c2ec2
Gori <wnsrjf0922@gmail.com>
2023-02-23 16:48:25 +0900

    [FEAT] Add broadcast (broadcast channel members)

--- includes/Utils.hpp
+++ includes/Utils.hpp
@@ -11,6 +11,9 @@
 #include <arpa/inet.h>
 #include <sys/select.h>
 #include <fcntl.h>
+#include "../includes/Server.hpp"
+
+class Channel;
 
 std::vector<std::string> string_split(std::string str, const char* delimiter);
 
@@ -19,6 +22,8 @@ std::string &ltrim(std::string &s, std::string delimiter);
 std::string &rtrim(std::string &s, std::string delimiter);
 
 const std::string &trim(std::string &s, std::string delimiter);
+
+void broadcast(std::map<std::string, Channel *> tempCh, std::string chName, std::string msg);
 
 void	reply(int fd, int replyNum, std::string msg);
 
