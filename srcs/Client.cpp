cccca9c121fcbf61cc616af2d7348d9386dc84b0
Gori <wnsrjf0922@gmail.com>
2023-02-25 16:50:56 +0900

    [REFACT] refact JOIN command

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -14,7 +14,7 @@ Client::Client(int fd)
 	cmdList["USER"] = cmdUser;
 	cmdList["NICK"] = cmdNick;
 	cmdList["PRIVMSG"] = cmdPrivMsg;
-	// cmdList["JOIN"] = cmdJoin;
+	cmdList["JOIN"] = cmdJoin;
 	// cmdList["KICK"] = cmdKick;
 }
 
