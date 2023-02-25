88f26ffa560953735ebdacdf5860b8f71998d958
objectio <objection@kakao.com>
2023-02-25 17:19:20 +0900

    [FEAT] Implement numeric replies

--- srcs/command/Nick.cpp
+++ srcs/command/Nick.cpp
@@ -2,10 +2,24 @@
 
 void	cmdNick(Command cmd, int fd)
 {
-	// if (str.size() < 1) // ERR_NONICKNAMEGIVEN 431
 	std::vector<std::string> params = cmd.getParams();
 	Client &c = cmd.getServer().getClient(fd);
 
+	if (params.size() < 1) 
+	{
+		reply(fd, 431, NULL);
+		return;
+	}
+	if (params[0].size() > 9)
+	{
+		reply(fd, 432, params[0]);
+		return;
+	}
+	if (cmd.getServer().isAlreadyUsed(params[0]))
+	{
+		reply(fd, 433, params[0]);
+		return;
+	}
 	if (c.getUserState() == REGISTER)
 	{
 		c.setNickName(params[0]);
