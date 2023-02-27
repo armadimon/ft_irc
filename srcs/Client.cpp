6ee0046aca853aef0a165cded893b1c0f8272f91
objectio <objection@kakao.com>
2023-02-27 11:07:12 +0900

    [CHORE] Delete cmd function prototypes

--- srcs/Client.cpp
+++ srcs/Client.cpp
@@ -1,11 +1,5 @@
 #include "../includes/Client.hpp"
 
-void	cmdPass(Command cmd, int fd);
-void	cmdUser(Command cmd, int fd);
-void	cmdNick(Command cmd, int fd);
-void	cmdPrivMsg(Command cmd, int fd);
-void	cmdJoin(Command cmd, int fd);
-
 Client::Client(int fd)
 	:fd(fd), userState(DEFAULT)
 {
@@ -15,7 +9,7 @@ Client::Client(int fd)
 	cmdList["NICK"] = cmdNick;
 	cmdList["PRIVMSG"] = cmdPrivMsg;
 	cmdList["JOIN"] = cmdJoin;
-	// cmdList["KICK"] = cmdKick;
+	cmdList["KICK"] = cmdKick;
 }
 
 Client::Client() {}
